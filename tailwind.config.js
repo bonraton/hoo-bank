8bd5b171f21291b76fd367ac454ed3e5fd695677
bonraton <bonraton@yandex.ru>
2023-04-19 21:20:25 +0200

    feat: header: git push origin develop

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        main: '#00040F',
-        'main-gradient':
-          'linear-gradient(110.53deg, #FFFFFF -265.46%, #14101D 96.75%)',
-        'accent-gradient':
-          'linear-gradient(157.81deg, #DEF9FA -43.27%, #BEF3F5 -21.24%, #9DEDF0 12.19%, #7DE7EB 29.82%, #5CE1E6 51.94%, #33BBCF 90.29%);',
-      },
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151 124, 0.1)",
     },
+    fontFamily: {
+      poppins: ["Poppins, sans-serif"]
+    }
   },
+  screens: {
+    xs: '480px',
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px",
+  }
+},
   plugins: [],
 };
